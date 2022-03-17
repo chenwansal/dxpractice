bdb9ea51086bd49b1fd434eeaeaf320b58e2be0f
杰克有茶 <chenwan@xmfunny.com>
2022-03-17 10:11:25 +0800

    <feature> 初始化WICLoader

--- WinMain.cpp
+++ WinMain.cpp
@@ -6,6 +6,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                       _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
                       _In_ int nCmdShow) {
 
+    HRESULT hr = CoInitialize(NULL);
+    if (FAILED(hr)) {
+        PLogger::PopupErrorWithResult(hr, "Failed To Init WICLOADER");
+        return -1;
+    }
+
     EngineCore engine;
     if (engine.Initialize(hInstance, "myTitle", "myClass", 800, 800)) {
         while (engine.Process()) {
